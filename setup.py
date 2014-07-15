618fb69b0814dddf52aa01030e231164611fc9ba
Patrick Michaud <pmichaud@uw.edu>
2014-07-15 10:23:14 -0700

    Adding setup.py for pip

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+setup(
+    name="DjangoNullCommand",
+    version="1.0",
+    packages=[ 'null_command' ],
+    author = "Patrick Michaud",
+    author_email = "pmichaud@uw.edu",
+    description = "Adds a management command that successfully does nothing",
+    license = "Apache 2.0",
+    keywords = "",
+)
